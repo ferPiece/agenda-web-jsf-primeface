c385f95fe83089b44859b0150586fd0aa4e11e7b
Fernando Lopez <flopezgonzalez546@gmail.com>
2016-11-09 22:54:55 -0300

    definicionen en la estructura del proyecto

---
+++ src/java/controller/ContactoBean.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import model.Contacto;
+/**
+ *
+ * @author lopez
+ */
+@ManagedBean
+@SessionScoped
+public class ContactoBean implements Serializable {
+
+    private static final long serialVersionUID = 455659950717243338L;
+    private Contacto contacto = new Contacto();
+    private List<Contacto> contactos = new ArrayList<Contacto>();
+    
+    public ContactoBean() {
+        //cargar lista de contactos
+        //falta implementar
+    }
+    
+    public void agregarContacto(){
+        //falta implementar
+        contactos.add(contacto);
+        showMessage("Contacto " + contacto.getNombre() + " agregado con exito!");
+    }
+    
+    public void eliminarContacto(){
+        //falta implementar
+        contactos.remove(contacto);
+        showMessage("Contacto " + contacto.getNombre() + " eliminado con exito!");
+    }
+    
+    public void modificarContacto(){
+        //falta implementar
+        showMessage("Contacto " + contacto.getNombre() + " modificado con exito!");
+    }
+    
+    public void limpiar(){
+        this.contacto = new Contacto();
+    }
+    
+    private void showMessage(String mensaje) {
+        FacesContext context = FacesContext.getCurrentInstance();  
+        context.addMessage(null, new FacesMessage(mensaje));  
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+
+    public List<Contacto> getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(List<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+    
+    public Contacto getNullContacto(){
+        return new Contacto();
+    }
+    
+}
---
+++ src/java/model/Contacto.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author lopez
+ */
+@ManagedBean
+@RequestScoped
+public class Contacto {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String fecha_creacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public void setFecha_creacion(String fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+}
